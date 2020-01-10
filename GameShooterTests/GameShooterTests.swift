3e557565016e6fe80e1ec390196544b63cb9874d
liza.metla <liza.metla@gmail.com>
2020-01-11 01:20:04 +0300

    Initial Commit

---
+++ GameShooterTests/GameShooterTests.swift
@@ -0,0 +1,36 @@
+//
+//  GameShooterTests.swift
+//  GameShooterTests
+//
+//  Created by Елизавета Метла on 11.01.2020.
+//  Copyright © 2020 Елизавета Метла. All rights reserved.
+//
+
+import XCTest
+@testable import GameShooter
+
+class GameShooterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
